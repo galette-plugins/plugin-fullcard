234268b7e509cd133d33c96aadcd423f2e5aa8fd
Johan Cwiklinski <johan@x-tnd.be>
2026-04-20 09:54:53 +0200

    Add plugin class

---
+++ lib/GaletteFullcard/PluginGaletteFullcard.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright © 2021-2026 The Galette Team
+ *
+ * This file is part of Galette OAuth2 plugin (https://galette-community.github.io/plugin-oauth2/).
+ *
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Galette OAuth2 plugin. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace GaletteFullcard;
+
+use Galette\Core\GalettePlugin;
+
+/**
+ * Galette OAuth2 plugin main class
+ *
+ * @author Johan Cwiklinski <johan@x-tnd.be>
+ */
+
+class PluginGaletteFullcard extends GalettePlugin
+{
+    /**
+     * Is the plugin fully installed (including database, extra configuration, etc.)?
+     */
+    public function isInstalled(): bool
+    {
+        return true;
+    }
+}
